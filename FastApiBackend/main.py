f5c45692a1787e02df88cb03ed8b090e93ca6df6
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2025-06-11 15:31:52 -0600

    Comentarios .gitignore y variables de entorno

--- FastApiBackend/main.py
+++ FastApiBackend/main.py
@@ -74,10 +74,14 @@ async def healthcheck():
 
     return {"status": status}
 
-
+#Micoservicio de admin
 from Admin.api.routes import router as admin_router
 app.include_router(admin_router, prefix='/admin/v1', tags=['admin'])
+
+#Microservicio para administracio de huespedes y accesos
 from Huesped.api.routes import router as huesped_router
 app.include_router(huesped_router, prefix='/huesped/v1', tags=['huesped'])
+
+#Microservicio de autenticacion
 from Auth.api.routes import router as auth_router
 app.include_router(auth_router, prefix='/auth/v1', tags=['auth'])
